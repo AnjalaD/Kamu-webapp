bb31c50ee2c86761db5734609e2e61f46350d5a9
AnjalaD <anjaladilhara@gmail.com>
2019-02-15 00:44:04 +0530

    php added to html - login

--- login.php
+++ login.php
@@ -1,4 +1,43 @@
-<!DOCTYPE html>
+<?php
+
+require 'user-management/db.php';
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if (isset($_POST['login'])) {
+        // require 'user-management/login.php';
+        $email = $mysqli->escape_string($_POST['email']);
+        $result = $mysqli->query("SELECT * FROM users WHERE email='$email'");
+
+        if ($result->num_rows == 0){ //user dosent exist
+            $_SESSION['message'] = "User with this email dosent exist!";
+            header("location: error.php");
+
+        }else{  //user exists
+            $user = $result->fetch_assoc();
+
+            if (password_verify($_POST['password'], $user['password'])){
+                $_SESSION['email'] = $user['email'];
+                $_SESSION['first_name'] = $user['first_name'];
+                $_SESSION['last_name'] = $user['last_name'];
+                $_SESSION['active'] = $user['active'];
+
+                $_SESSION['logged_in'] = true;
+
+                $_SESSION['login_error_message'] = null;
+
+                header("location: kamu.php");
+
+            }else{
+                $_SESSION['login_error_message'] = "You have entered a wrong password!";
+                // header("location: login.php");
+            }
+        }
+    }
+}
+
+?>
+
 <html>
 
 <head>
@@ -36,18 +75,23 @@
             </div>
         </nav>
     </div>
+    <?php if( !empty($_SESSION['login_error_message'])){
+         echo $_SESSION['login_error_message'];
+    }
+    ?>
     <div class="login-clean">
         <form method="post">
             <h2 class="sr-only">Login Form</h2>
             <div class="illustration"><i class="icon ion-ios-navigate"></i></div>
             <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email"></div>
             <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
-            <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Log In</button></div><a href="#" class="forgot">Forgot your email or password?</a></form>
+            <div class="form-group"><button class="btn btn-primary btn-block" type="submit" name="login" id="login">Log In</button></div><a href="#" class="forgot">Forgot your email or password?</a></form>
     </div>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
     <script src="assets/js/Simple-Slider1.js"></script>
+
 </body>
 
 </html>
