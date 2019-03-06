b0e5a6aeb428024f5dabb5192978c8380c2223d3
Budvin Chathura <bc@example.com>
2019-03-06 22:11:42 +0530

    deleted

--- app/views/inc/scripts.php
+++
@@ -1,4 +0,0 @@
-<script src="/mcv/public/assets/js/jquery.min.js"></script>
-<script src="/mcv/public/assets/bootstrap/js/bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
-<script src="/mcv/public/assets/js/Simple-Slider1.js"></script>
--- app/views/user-management/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-// database connection settings
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'accounts';
-$mysqli = new mysqli($host, $user, $pass , $db) or die($myqli->error);
-
-?>
