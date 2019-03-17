92ed97e42752bc7b17e95758af3e14f8d3c4dc5f
AnjalaD <anjaladilhara@gmail.com>
2019-03-17 23:13:15 +0530

    login and register ui updated

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -4,31 +4,30 @@ use core\Session;
 
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title><?=$this->get_title() ?> </title>
+    <title><?= $this->get_title() ?> </title>
     <!-- Bootstrap CSS -->
-    
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
-    
-    <?= $this->content('head') ?>
-  </head>
 
-  <body>
+    <?= $this->content('head') ?>
+</head>
+
+<body>
     <?php include 'navbar.php' ?>
-    <div class="container" style="min-height:cal(100% - 125px);" >
-    <?=Session::display_msgs()?>
+    <?= Session::display_msgs() ?>
     <?= $this->content('body') ?>
-    </div>
-    
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
+</body>
+
+</html> 
--- app/views/register/register.php
+++ app/views/register/register.php
@@ -1,5 +1,6 @@
 <?php
 use core\FH;
+
 $token = FH::generate_token();
 ?>
 
@@ -7,11 +8,13 @@ $token = FH::generate_token();
 
 <?php $this->start('head'); ?>
 <script src="<?= SROOT ?>js/register_form_validate.js"></script>
+<link rel="stylesheet" href="<?= SROOT ?>css/styles.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Aclonica">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 <?php $this->end(); ?>
 
 <?php $this->start('body'); ?>
-<div class="col-md-6 col-md-offset-3 well">
-
+<div id="Register_LoginDark_Background" class="login-dark">
     <div class="nav nav-tabs" id="nav-tab" role="tablist">
         <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#user" role="tab" aria-controls="nav-home" aria-selected="true">Register as User</a>
         <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#owner" role="tab" aria-controls="nav-profile" aria-selected="false">Resgister as Owner</a>
@@ -19,72 +22,72 @@ $token = FH::generate_token();
 
     <div class="tab-content" id="nav-tabContent">
         <div class="tab-pane fade show active" id="user" role="tabpanel" aria-labelledby="nav-home-tab">
-            <form class="form" action="<?= SROOT ?>register/register/customer" method="post">
+
+            <form method="post" id="Register_Main_LoginBox" class="LoginBox" action="<?= SROOT ?>register/register/user">
+                <h2 class="sr-only">User-Register Form</h2>
                 <?= FH::csrf_input($token) ?>
                 <?= FH::display_errors($this->display_errors) ?>
-                <div class="form-group">
-                    <div>
-                        <label for="first_name">First name</label>
-                        <input type="text" name="first_name" id="first_name" class="form-control" required pattern="\w+" title="must not be blank and contain only letters, numbers and underscores" value="<?= $this->new_user->first_name ?>">
-                    </div>
-                    <div>
-                        <label for="last_name">Last name</label>
-                        <input type="text" name="last_name" id="last_name" class="form-control" required pattern="\w+" title="must not be blank and contain only letters, numbers and underscores" value="<?= $this->new_user->last_name ?>">
-                    </div>
-                    <div>
-                        <label for="email">Email</label>
-                        <input type="text" name="email" id="email" class="form-control" required pattern="^(([-\w\d]+)(\.[-\w\d]+)*@([-\w\d]+)(\.[-\w\d]+)*(\.([a-zA-Z]{2,5}|[\d]{1,3})){1,2})$" title="must be a valid email address" value="<?= $this->new_user->email ?>">
-                    </div>
-                    <div>
-                        <label for="password">Password</label>
-                        <input type="password" name="password" id="password" class="form-control" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" title="must contain at least 6 characters, including UPPER/lowercase and numbers" value="">
-                    </div>
-                    <div>
-                        <label for="confirm">Confirm Password</label>
-                        <input type="tex" name="confirm" id="confirm" class="form-control" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" title="should match with the password entered above" value="">
-                    </div>
+                <div id="Logo_Illustration" class="illustration">
+                    <img src="<?= SROOT ?>assets/img/150monoLogoOnlyKamu.png">
                 </div>
-                <div class="form-group">
-                    <input type="submit" value="Register as User" class="btn btn-large btn-primary">
+                <div class="form-group" id="Register_Email_FormGroup">
+                    <input class="form-control TextInput" type="email" name="email" placeholder="Email" id="Register_Email_TextInput">
                 </div>
-                <div class="text-right">
-                    <a href="<?= SROOT ?>register/login" class="text-primary">login</a>
+                <div class="form-group" id="Register_FirstName_FormGroup">
+                    <!-- Start: #FirstName_TextInput --><input type="string" name="first_Name" placeholder="First Name" class="form-control TextInput" id="Register_Firstname_TextInput" />
+                    <!-- End: #FirstName_TextInput -->
+                </div>
+                <div class="form-group" id="Register_LastName_FormGroup">
+                    <!-- Start: #LastName_TextInput --><input type="string" name="last_name" placeholder="Last Name" class="form-control TextInput" id="Register_LastName_TextInput" />
+                    <!-- End: #LastName_TextInput -->
+                </div>
+                <div class="form-group" id="Register_Password_FormGroup">
+                    <input class="form-control TextInput" type="password" name="password" placeholder="Password" id="Register_Password_TextInput">
+                </div>
+                <div class="form-group" id="Register_ConfirmPassword_FormGroup">
+                    <!-- Start: #ConfirmPassword_TexInput --><input type="password" name="confirm" placeholder="Confirm Password" class="form-control TextInput" id="Register_ConfirmPassword_TextInput" />
+                    <!-- End: #ConfirmPassword_TexInput -->
+                </div>
+                <div class="form-group" id="Button_FormGroup">
+                    <!-- Start: #RegisterButton --><button class="btn btn-primary btn-block Button" type="submit" id="Register_Button">Register as User</button>
+                    <!-- End: #RegisterButton -->
                 </div>
             </form>
+
         </div>
         <div class="tab-pane fade" id="owner" role="tabpanel" aria-labelledby="nav-profile-tab">
-            <form class="form" action="<?= SROOT ?>register/register/owner" method="post">
+
+            <form method="post" id="Register_Main_LoginBox" class="LoginBox" action="<?= SROOT ?>register/register/owner">
+                <h2 class="sr-only">Owner-Register Form</h2>
                 <?= FH::csrf_input($token) ?>
                 <?= FH::display_errors($this->display_errors) ?>
-                <div class="form-group">
-                    <div>
-                        <label for="first_name">First name</label>
-                        <input type="text" name="first_name" id="first_name" class="form-control" required pattern="\w+" title="must not be blank and contain only letters, numbers and underscores" value="<?= $this->new_user->first_name ?>">
-                    </div>
-                    <div>
-                        <label for="last_name">Last name</label>
-                        <input type="text" name="last_name" id="last_name" class="form-control" required pattern="\w+" title="must not be blank and contain only letters, numbers and underscores" value="<?= $this->new_user->last_name ?>">
-                    </div>
-                    <div>
-                        <label for="email">Email</label>
-                        <input type="text" name="email" id="email" class="form-control" required pattern="^(([-\w\d]+)(\.[-\w\d]+)*@([-\w\d]+)(\.[-\w\d]+)*(\.([a-zA-Z]{2,5}|[\d]{1,3})){1,2})$" title="must be a valid email address" value="<?= $this->new_user->email ?>">
-                    </div>
-                    <div>
-                        <label for="password">Password</label>
-                        <input type="password" name="password" id="password" class="form-control" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" title="must contain at least 6 characters, including UPPER/lowercase and numbers" value="">
-                    </div>
-                    <div>
-                        <label for="confirm">Confirm Password</label>
-                        <input type="tex" name="confirm" id="confirm" class="form-control" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" title="should match with the password entered above" value="">
-                    </div>
+                <div id="Logo_Illustration" class="illustration">
+                    <img src="<?= SROOT ?>assets/img/150monoLogoOnlyKamu.png">
                 </div>
-                <div class="form-group">
-                    <input type="submit" value="Register as Owner" class="btn btn-large btn-primary">
+                <div class="form-group" id="Register_Email_FormGroup">
+                    <input class="form-control TextInput" type="email" name="email" placeholder="Email" id="Register_Email_TextInput">
                 </div>
-                <div class="text-right">
-                    <a href="<?= SROOT ?>register/login" class="text-primary">login</a>
+                <div class="form-group" id="Register_FirstName_FormGroup">
+                    <!-- Start: #FirstName_TextInput --><input type="string" name="first_Name" placeholder="First Name" class="form-control TextInput" id="Register_Firstname_TextInput" />
+                    <!-- End: #FirstName_TextInput -->
+                </div>
+                <div class="form-group" id="Register_LastName_FormGroup">
+                    <!-- Start: #LastName_TextInput --><input type="string" name="last_name" placeholder="Last Name" class="form-control TextInput" id="Register_LastName_TextInput" />
+                    <!-- End: #LastName_TextInput -->
+                </div>
+                <div class="form-group" id="Register_Password_FormGroup">
+                    <input class="form-control TextInput" type="password" name="password" placeholder="Password" id="Register_Password_TextInput">
+                </div>
+                <div class="form-group" id="Register_ConfirmPassword_FormGroup">
+                    <!-- Start: #ConfirmPassword_TexInput --><input type="password" name="confirm" placeholder="Confirm Password" class="form-control TextInput" id="Register_ConfirmPassword_TextInput" />
+                    <!-- End: #ConfirmPassword_TexInput -->
+                </div>
+                <div class="form-group" id="Button_FormGroup">
+                    <!-- Start: #RegisterButton --><button class="btn btn-primary btn-block Button" type="submit" id="Register_Button">Register as Owner</button>
+                    <!-- End: #RegisterButton -->
                 </div>
             </form>
+
         </div>
     </div>
 </div>
