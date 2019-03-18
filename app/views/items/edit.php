86f3d58ed61dfc16cc749ccfff4f4a3d7e88151e
AnjalaD <anjaladilhara@gmail.com>
2019-03-18 18:05:48 +0530

    refactor coppie function

--- app/views/items/edit.php
+++ app/views/items/edit.php
@@ -13,45 +13,5 @@
 
 <?php $this->start('script'); ?>
 <script src="<?= SROOT ?>js/croppie.js"></script>
-<script>  
-    $(document).ready(function(){
-    
-        $image_crop = $('#image_demo').croppie({
-        enableExif: true,
-        viewport: {
-          width:200,
-          height:200,
-          type:'square' //circle
-        },
-        boundary:{
-          width:300,
-          height:300
-        }
-      });
-    
-      $('#upload_image').on('change', function(){
-        var reader = new FileReader();
-        reader.onload = function (event) {
-          $image_crop.croppie('bind', {
-            url: event.target.result
-          }).then(function(){
-            console.log('jQuery bind complete');
-          });
-        }
-        reader.readAsDataURL(this.files[0]);
-        $('#uploadimageModal').modal('show');
-      });
-    
-      $('.crop_image').click(function(event){
-        $image_crop.croppie('result', {
-          type: 'canvas',
-          size: 'viewport'
-        }).then(function(response){
-          $('#image').attr('value', response);
-          $('#uploadimageModal').modal('hide');
-        })
-      });
-    
-    });  
-    </script>
+<script src="<?= SROOT ?>js/croppie-function.js"></script>
 <?php $this->end(); ?>
--- app/views/restaurant/add.php
+++ app/views/restaurant/add.php
@@ -10,76 +10,9 @@
     <?php $this->partial('restaurant', 'form'); ?>
 </div>
 <div id="uploaded_image"></div>
-
-<div id="uploadimageModal" class="modal" role="dialog">
-	<div class="modal-dialog">
-		<div class="modal-content">
-      		<div class="modal-header">
-        		<button type="button" class="close" data-dismiss="modal">&times;</button>
-        		<h4 class="modal-title">Upload & Crop Image</h4>
-      		</div>
-      		<div class="modal-body">
-        		<div class="row">
-  					<div class="col-md-8 text-center">
-						  <div id="image_demo" style="width:350px; margin-top:30px"></div>
-  					</div>
-  					<div class="col-md-4" style="padding-top:30px;">
-  						<br />
-  						<br />
-  						<br/>
-						  <button class="btn btn-success crop_image">Crop & Upload Image</button>
-					</div>
-				</div>
-      		</div>
-      		<div class="modal-footer">
-        		<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      		</div>
-    	</div>
-    </div>
-</div>
 <?php $this->end();?>
 
 <?php $this->start('script');?>
 <script src="<?= SROOT ?>js/croppie.js"></script>
-<script>  
-    $(document).ready(function(){
-    
-        $image_crop = $('#image_demo').croppie({
-        enableExif: true,
-        viewport: {
-          width:400,
-          height:400,
-          type:'square' //circle
-        },
-        boundary:{
-          width:450,
-          height:450
-        }
-      });
-    
-      $('#upload_image').on('change', function(){
-        var reader = new FileReader();
-        reader.onload = function (event) {
-          $image_crop.croppie('bind', {
-            url: event.target.result
-          }).then(function(){
-            console.log('jQuery bind complete');
-          });
-        }
-        reader.readAsDataURL(this.files[0]);
-        $('#uploadimageModal').modal('show');
-      });
-    
-      $('.crop_image').click(function(event){
-        $image_crop.croppie('result', {
-          type: 'canvas',
-          size: 'viewport'
-        }).then(function(response){
-          $('#image').attr('value', response);
-          $('#uploadimageModal').modal('hide');
-        })
-      });
-    
-    });  
-    </script>
+<script src="<?= SROOT ?>js/croppie-function.js"></script>
 <?php $this->end(); ?>
