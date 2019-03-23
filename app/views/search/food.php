6dcf6ee720b03d1cbeccbaa58c4fcad110ada6ca
AnjalaD <anjaladilhara@gmail.com>
2019-03-23 22:51:07 +0530

    refactor

--- app/views/search/food.php
+++ app/views/search/food.php
@@ -1,47 +1,36 @@
 <?php
 use core\FH;
 ?>
-<?php $this->set_title('Home'); ?>
+
+<?php $this->set_title('Food'); ?>
+
 <?php $this->start('head'); ?>
-<link rel="stylesheet" href="<?=SROOT?>css/foodstyles.min.css">
+<link rel="stylesheet" href="<?= SROOT ?>css/foodstyles.min.css">
 <?php $this->end(); ?>
 
 <?php $this->start('body'); ?>
+
 <div class="container-fluid">
-    <form method="POST" action="<?=SROOT?>search">
+    <form method="POST" action="<?= SROOT ?>search">
         <div class="input-group">
-                <input type="text" autocomplete="off" class="form-control" list="food" name="search_string" id="search" value="<?=$this->post_data?>" placeholder="Enter what you want">
-                <div class="input-group-append">
-                    <input type="submit" class="btn btn-outline-secondary" value="Search" name="food" id="search">
-                </div>
+            <input type="text" autocomplete="off" class="form-control" list="food" name="search_string" id="search" value="<?= $this->post_data ?>" placeholder="Enter what you want">
+            <div class="input-group-append">
+                <input type="submit" class="btn btn-outline-secondary" value="Search" name="food" id="search">
+            </div>
             <datalist id="food"></datalist>
         </div>
     </form>
 </div>
 
-<?=$this->results?>
-
+<?= $this->results ?>
 <?php $this->end(); ?>
 
-<?php $this->start('script')?>
+<?php $this->start('script') ?>
+<script src="<?= SROOT ?>js/functions.js"></script>
 <script>
     var search = document.getElementById('search');
-    search.onkeyup = function () {
-        $.ajax({
-            type: "POST",
-            url: '<?= SROOT ?>search/auto_complete/' + search.value,
-            data: {
-                model_id: 45
-            },
-            success: function (resp) {
-                temp = '';
-                for (i = 0; i < resp.length; i++) {
-                    temp += '<option class="list-group-item" value="'+resp[i]+ '">' + resp[i] + '</option>';
-                }
-                document.getElementById('food').innerHTML = (temp);
-                // console.log(resp);
-            }
-        });
-    }
+    search.onkeyup = function() {
+        autoComplete(search, 'food');
+    };
 </script>
-<?php $this->end(); ?>
+<?php $this->end(); ?> 
