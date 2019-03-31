c987041a45b3c11c7622d492988073ca414b9a33
Budvin Chathura <chathura97rc@gmail.com>
2019-03-31 19:57:41 +0530

    addtoorder js

--- js/addtoorder.js
+++ js/addtoorder.js
@@ -6,6 +6,15 @@ function addToOrder(rid, id,element) {
         },
         success: function(resp) {
             console.log(resp);
+            if(resp=='1'){
+                element.innerHTML = 'Remove Item';
+            }
+            else if(resp=='0'){
+                window.alert('Please select food from one restaurant');
+            }
+            else if(resp=='-1'){
+                window.alert('Please Login');
+            }
             if(!resp) window.alert('Please select food from one restaurant');
         }
     });
