a295f8433ddd93d0a8cb4d8a3b4c5dfa28430d3f
Budvin Chathura <chathura97rc@gmail.com>
2019-03-31 18:45:55 +0530

    public config

--- config/config.php
+++ config/config.php
@@ -12,10 +12,7 @@ define('DB_HOST', 'localhost:3306');
 define('DEFAULT_CONTROLLER', 'Home'); //if no controller is set Route will use this
 define('DEFAULT_LAYOUT', 'default'); //if no layout is set controller use this
 
-define('SROOT', '/Kamu_1.0/exported/'); //set this for / for a live server
-// define('SROOT', '/mvc/'); //set this for / for a live server
-define('WEB_ADDRESS', 'https://localhost/Kamu_1.0/exported/');
-// define('WEB_ADDRESS', 'https://localhost/mvc/');
+
 define('SITE_TITLE', 'MVC framwork'); //default web page title
 define('BRAND_NAME','Kamu');
 
