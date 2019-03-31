ee18b70298ab300eefe0963d6ba52a96c7a3cdb5
AnjalaD <anjaladilhara@gmail.com>
2019-03-31 18:40:27 +0530

    check

--- config/private-config.php
+++ config/private-config.php
@@ -1,6 +1,4 @@
 <?php
 
-// define('SROOT', '/Kamu_1.0/exported/'); //set this for / for a live server
 define('SROOT', '/mvc/'); //set this for / for a live server
-// define('WEB_ADDRESS', 'https://localhost/Kamu_1.0/exported/');
 define('WEB_ADDRESS', 'https://localhost/mvc/');
