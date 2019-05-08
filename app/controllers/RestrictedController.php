d155824ad916e6c99eae6dd64cb4e55547fda5eb
AnjalaD <anjaladilhara@gmail.com>
2019-05-08 22:40:18 +0530

    update - error pages

--- app/controllers/RestrictedController.php
+++ app/controllers/RestrictedController.php
@@ -9,13 +9,24 @@ class RestrictedController extends Controller
         parent::__construct($controller, $acttion);
     }
 
-    public function index_action()
+    public function no_permission_action()
     {
-        $this->view->render('restricted/index');
+        $this->view->render('restricted/no_permission');
     }
 
     public function error_action()
     {
         $this->view->render('restricted/error');
+    }
+
+    public function invalid_token_action()
+    {
+        $this->view->render('restricted/invalid_token');
+    }
+
+    public function page_not_found_action($info)
+    {
+        $this->view->info = $info;
+        $this->view->render('restricted/page_not_found');
     }
 }
