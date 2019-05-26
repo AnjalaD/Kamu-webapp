e8daca47224c30904de1eff806266f08d4e7fb82
Thumula Perera <thumulaperera@gmail.com>
2019-05-27 00:24:01 +0530

    conflicts

--- config/private-config.php
+++ config/private-config.php
@@ -1,8 +1,3 @@
 <?php
-<<<<<<< HEAD
 define('SROOT','/Kamu/Kamu-webapp/');
 define('WEB_ADDRESS','https://localhost/Kamu/Kamu-webapp/');
-=======
-define('SROOT','/Kamu_1.0/exported/');
-define('WEB_ADDRESS','https://localhost/Kamu_1.0/exported/');
->>>>>>> 931e393ce6bf1998c5981e86cf215f79e8c38f14
