49e37f243c887ff4f8771608eaeef7a9ccf7c2fc
Budvin Chathura <chathura97rc@gmail.com>
2019-05-27 15:40:34 +0530

    order submitted session msg

--- app/controllers/OrderController.php
+++ app/controllers/OrderController.php
@@ -223,6 +223,7 @@ class OrderController extends Controller
 
             if($new_order->save() && $new_submitted_order->save())
             {
+                Session::add_msg('success', 'Order Submitted!!!');
                 Router::redirect('');
             }
             $this->view->post_data = $new_order;
