306bdc5db1baab4e7a33bb0b79ca233cb82f5bf8
Thumula Perera <thumulaperera@gmail.com>
2019-05-27 16:20:47 +0530

    apply filters button refactor

--- app/views/items/index.php
+++ app/views/items/index.php
@@ -10,7 +10,7 @@ use core\H;
 <?php $this->end(); ?>
 
 <?php $this->start('body'); ?>
-<div class="Profile_body" style="background-image: url(&quot;<?=SROOT?>assets/img/profile_background.jpg&quot;); background-position: center; background-repeat: no-repeat; background-size: cover; height: 100%; font-family:Aclonica;">
+<div class="Profile_body" style="background-image: url(&quot;<?=SROOT?>assets/img/profile_background.jpg&quot;); background-position: center; background-repeat: no-repeat; background-size: cover; height: 100%; font-family:Aclonica; min-width:60rem;">
     <div class="card card-cascade narrower m-3" style="background:rgb(255,255,255,.7);">
         <div class="card-header py-2 mx-4 mb-3 d-flex justify-content-between align-items-center" style="background:#9d2525; color:white;">
             <div></div>    
@@ -23,8 +23,8 @@ use core\H;
                     <thead>
                         <tr>
                             <th class="font-weight-bold" style="width:20%; font-size:1.25rem;">Food Item</th>
-                            <th class="font-weight-bold" style="width:220px; font-size:1.25rem;">Image</th>
-                            <th class="font-weight-bold" style="width:45%; font-size:1.25rem;">Description</th>
+                            <th class="font-weight-bold" style="width:15%; font-size:1.25rem;">Image</th>
+                            <th class="font-weight-bold" style="width:30%; font-size:1.25rem;">Description</th>
                             <th class="font-weight-bold" style="width:15%; font-size:1.25rem;">Price</th>
                             <th style="width:20%;"></th>
                         </tr>
@@ -33,7 +33,7 @@ use core\H;
                         <?php foreach ($this->items as $item) : ?>
                             <tr>
                                 <td><button  class="btn btn-primary item "  data-toggle="modal" data-target="#item_preview" id="<?= $item->id ?>"><?= $item->item_name ?></button></td>
-                                <td><img src="<?= $item->image_url ?>" style="object-fit:contain;"></td>
+                                <td><img src="<?= $item->image_url ?>" style="object-fit:contain; width:100%;"></td>
                                 <td style="font-size:1.05rem;"><?= $item->description ?></td>
                                 <td class="font-weight-bold" style="font-size:1.05rem;"><?= number_format((float)round($item->price,2), 2, '.', '') ?> LKR</td>
                                 <td class="text-right">
