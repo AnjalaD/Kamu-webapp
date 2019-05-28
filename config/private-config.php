cd6fa80195e64e5d66c951891e05402acb0671bb
Thumula Perera <thumulaperera@gmail.com>
2019-05-28 22:09:51 +0530

    conflicts

--- config/private-config.php
+++ config/private-config.php
@@ -1,8 +1,3 @@
 <?php
-<<<<<<< HEAD
 define('SROOT','/Kamu/Kamu-webapp/');
 define('WEB_ADDRESS','https://localhost/Kamu/Kamu-webapp/');
-=======
-define('SROOT','/mvc/');
-define('WEB_ADDRESS','https://localhost/mvc/');
->>>>>>> 873cb29dd424fa35d5c206d741ccd38b2bcf12d3
--- js/config.js
+++ js/config.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 const SROOT = '/Kamu/Kamu-webapp/'
-=======
-const SROOT = '/mvc/'
->>>>>>> 873cb29dd424fa35d5c206d741ccd38b2bcf12d3
