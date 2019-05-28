15daeb215900027771385506f59366784711ce20
chamikaCN <chamikanandasiri97@gmail.com>
2019-05-29 01:19:23 +0530

    res card bug fix

--- config/private-config.php
+++ config/private-config.php
@@ -1,3 +1,3 @@
 <?php
-define('SROOT','/Kamu/Kamu-webapp/');
-define('WEB_ADDRESS','https://localhost/Kamu/Kamu-webapp/');
+define('SROOT','/KamuWebApp/');
+define('WEB_ADDRESS','https://localhost/KamuWebApp/');
