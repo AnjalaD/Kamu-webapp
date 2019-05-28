90255b39cb6ce55d8fe18ec552cde10efb04b31e
Budvin Chathura <chathura97rc@gmail.com>
2019-05-28 12:22:18 +0530

    register controller cashier model add

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -17,6 +17,7 @@ class RegisterController extends Controller
         $this->load_model('CustomerModel');
         $this->load_model('OwnerModel');
         $this->load_model('AdminModel');
+        $this->load_model('CashierModel');
         $this->view->set_layout('default');
     }
 
@@ -135,9 +136,13 @@ class RegisterController extends Controller
             $new_user->set_confirm($this->request->get('confirm'));
             
             // H::dnd($new_user);
+            // H::dnd($new_user);
             if($new_user ->save()) {
                 $new_user->send_verify_email();
+                Session::add_msg('success', 'Resgistration successful');
                 Router::redirect($redirect);
+            }else{
+                Session::add_msg('danger', 'Error! Could not register');
             }
         }
         $this->view->new_user = $new_user;
