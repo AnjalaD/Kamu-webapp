69d115d96244ba163596f0d1262ddae06a8acb28
Budvin Chathura <chathura97rc@gmail.com>
2019-05-28 21:44:46 +0530

    add item form refactor

--- app/views/items/partials/form.php
+++ app/views/items/partials/form.php
@@ -42,24 +42,19 @@ use core\FH;
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <button type="button" class="close" data-dismiss="modal" style="color:red;">&times;</button>
                 <h4 class="modal-title">Upload & Crop Image</h4>
             </div>
             <div class="modal-body">
-                <div class="row">
-                    <div class="col-md-8 text-center">
+                <div class="row text-center">
+                    <div class="text-center">
                         <div id="image_demo" style="width:350px; margin-top:30px"></div>
                     </div>
-                    <div class="col-md-4" style="padding-top:30px;">
-                        <br/>
-                        <br/>
-                        <br/>
-                        <button class="btn btn-success crop_image">Crop & Upload Image</button>
-                    </div>
+                    
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button class="btn btn-success crop_image">Crop & Upload Image</button>
             </div>
         </div>
     </div>
